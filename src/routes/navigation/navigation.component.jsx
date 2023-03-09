2c9372e2ac538131e8505c047740887278f7ef8a
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-03-09 06:04:02 +0000

    Done with Sagas, Starting Redux Toolkit

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import {
   LogoContainer,
@@ -9,19 +10,20 @@ import {
 } from "./navigation.styles";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { useSelector } from "react-redux";
 
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { selectIsCartOpen } from "../../store/cart/cart.selector";
+import { signOutStart } from "../../store/user/user.action";
 
 const Navigation = () => {
-  // const activeStyle = { borderBottom: "2px solid black" };
+  const dispatch = useDispatch()
 
   const currentUser  = useSelector(selectCurrentUser);
   const isCartOpen  = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutStart())
 
   return (
     <Fragment>
